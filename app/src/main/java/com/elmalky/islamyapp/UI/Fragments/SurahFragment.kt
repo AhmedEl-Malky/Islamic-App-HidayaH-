98ae10bd61d98f3d7e4742700e9b13a6384bf436
Ahmed Ramadan El_Malky <Ahmed.RamadanCS21@fcai.usc.edu.eg>
2024-02-03 21:33:51 +0200

    finish SurahFragment's UI

---
+++ app/src/main/java/com/elmalky/islamyapp/UI/Fragments/SurahFragment.kt
@@ -0,0 +1,19 @@
+package com.elmalky.islamyapp.UI.Fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.elmalky.islamyapp.R
+
+class SurahFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        var binder =
+            return inflater.inflate(R.layout.fragment_surah, container, false)
+    }
+
+}
