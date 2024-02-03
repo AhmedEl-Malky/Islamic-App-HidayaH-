72d6eb9876bf5fae79450c54b2bf7d94ff3ea23a
Ahmed Ramadan El_Malky <Ahmed.RamadanCS21@fcai.usc.edu.eg>
2024-02-03 21:30:06 +0200

    initialize the project and organize packages

---
+++ app/src/main/java/com/elmalky/islamyapp/UI/Activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.elmalky.islamyapp.UI.Activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.elmalky.islamyapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
