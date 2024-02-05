64f68044ec1035ffeaf19c6c18556a24aef42424
Ahmed Ramadan El_Malky <Ahmed.RamadanCS21@fcai.usc.edu.eg>
2024-02-05 15:58:46 +0200

    finish Quran Content Fragment UI

---
+++ app/src/main/java/com/elmalky/islamyapp/UI/Fragments/Sib7aFragment.kt
@@ -0,0 +1,21 @@
+package com.elmalky.islamyapp.UI.Fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.elmalky.islamyapp.R
+
+
+class Sib7aFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_sib7a, container, false)
+    }
+
+
+}
