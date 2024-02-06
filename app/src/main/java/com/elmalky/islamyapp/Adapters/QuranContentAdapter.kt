d6b63a2fcf5c87d49d9c716a3e625ba36839ee06
Ahmed Ramadan El_Malky <Ahmed.RamadanCS21@fcai.usc.edu.eg>
2024-02-06 22:26:01 +0200

    create quran content adapter

---
+++ app/src/main/java/com/elmalky/islamyapp/Adapters/QuranContentAdapter.kt
@@ -0,0 +1,34 @@
+package com.elmalky.islamyapp.Adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.elmalky.islamyapp.Interfaces.SurahItemInteractions
+import com.elmalky.islamyapp.R
+import com.elmalky.islamyapp.databinding.SurahItemBinding
+
+class QuranContentAdapter(val quranContent: List<String>) :
+    RecyclerView.Adapter<QuranContentAdapter.SurahItemHolder>(), SurahItemInteractions {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SurahItemHolder {
+        var view = LayoutInflater.from(parent.context).inflate(R.layout.surah_item, parent, false)
+        return SurahItemHolder(view)
+    }
+
+    override fun getItemCount() = quranContent.size
+
+    override fun onBindViewHolder(holder: SurahItemHolder, position: Int) {
+        holder.binder.apply {
+            surahName.text = quranContent[position]
+            surahNum.text = (position + 1).toString()
+        }
+    }
+
+    class SurahItemHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val binder = SurahItemBinding.bind(view)
+    }
+
+    override fun onItemClickListener(position: Int) {
+
+    }
+}
--- app/src/main/java/com/elmalky/islamyapp/UI/Fragments/QuranContentFragment.kt
+++ app/src/main/java/com/elmalky/islamyapp/UI/Fragments/QuranContentFragment.kt
@@ -5,17 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.elmalky.islamyapp.Adapters.QuranContentAdapter
+import com.elmalky.islamyapp.Util.Constants
 import com.elmalky.islamyapp.databinding.FragmentQuranContentBinding
 
 
 class QuranContentFragment : Fragment() {
-
+    lateinit var binder: FragmentQuranContentBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        var binder = FragmentQuranContentBinding.inflate(inflater, container, false)
+        binder = FragmentQuranContentBinding.inflate(inflater, container, false)
         return binder.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val adapter = QuranContentAdapter(Constants.Content.quranContent)
+        binder.quranContentRecycler.adapter = adapter
     }
 
 }
