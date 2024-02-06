aefcc6a7b89b31de612a612968221c82e5979c2f
Ahmed Ramadan El_Malky <Ahmed.RamadanCS21@fcai.usc.edu.eg>
2024-02-06 13:56:31 +0200

    finish azkar fragment.

---
+++ app/src/main/java/com/elmalky/islamyapp/UI/Fragments/AzkarFragment.kt
@@ -0,0 +1,20 @@
+package com.elmalky.islamyapp.UI.Fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.elmalky.islamyapp.R
+
+class AzkarFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_azkar, container, false)
+    }
+
+}
