a56e9d14eced1a3a29b07eb71197c6bf0701318d
Ahmed Ramadan El_Malky <Ahmed.RamadanCS21@fcai.usc.edu.eg>
2024-02-07 10:26:52 +0200

    finish azkar saba7 adapter with all functionality

--- app/src/main/java/com/elmalky/islamyapp/UI/Activities/AzkarSaba7Activity.kt
+++ app/src/main/java/com/elmalky/islamyapp/UI/Activities/AzkarSaba7Activity.kt
@@ -2,13 +2,29 @@ package com.elmalky.islamyapp.UI.Activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.elmalky.islamyapp.Adapters.AzkarSaba7Adapter
+import com.elmalky.islamyapp.Interfaces.AzkarItemInteractions
+import com.elmalky.islamyapp.Util.Constants
 import com.elmalky.islamyapp.databinding.ActivityAzkarSaba7Binding
 
-class AzkarSaba7Activity : AppCompatActivity() {
+class AzkarSaba7Activity : AppCompatActivity(), AzkarItemInteractions {
+    lateinit var adapter: AzkarSaba7Adapter
     lateinit var binder: ActivityAzkarSaba7Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binder = ActivityAzkarSaba7Binding.inflate(layoutInflater)
         setContentView(binder.root)
+        adapter = AzkarSaba7Adapter(Constants.DataManager.azkarSaba7, this)
+        binder.apply {
+            azkarSaba7BackBtn.setOnClickListener {
+                finish()
+            }
+            azkarSaba7RecyclerView.adapter = adapter
+        }
+    }
+
+    override fun onItemDeleteListener(position: Int) {
+        Constants.DataManager.deleteAzkarAt(position)
+        adapter.updateData(Constants.DataManager.azkarSaba7)
     }
 }
