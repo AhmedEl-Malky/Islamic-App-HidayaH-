93cfa89a4b3bb9012315f3e340cb56bec237e0b7
Ahmed  El_Malky <130024306+AhmedEl-Malky@users.noreply.github.com>
2024-02-10 17:13:30 +0200

    Feature/fix responsiveness (#29)
    * fix responsiveness
    
    * fix SplashScreen

--- app/src/main/java/com/elmalky/islamyapp/UI/Activities/SplashScreen.kt
+++ app/src/main/java/com/elmalky/islamyapp/UI/Activities/SplashScreen.kt
@@ -18,6 +18,7 @@ class SplashScreen : AppCompatActivity() {
         Handler().postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
+            finish()
         }, 2000)
     }
 }
